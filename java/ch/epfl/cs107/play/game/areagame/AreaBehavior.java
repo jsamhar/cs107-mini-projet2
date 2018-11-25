2701fbe9297636eab36437e5cbb797a0d1d02e79
Mathlpaca <heloise.piguet@gmail.com>
2018-11-25 20:45:13 +0100

    Updates 4.2

--- java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
+++ java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
@@ -9,6 +9,13 @@ public abstract class AreaBehavior
 {
 
     /// The behavior is an Image of size height x width
+	private final Image behaviorMap;
+	private final int width, height;
+	
+	/// we will convert the image into an array of cells
+	private final Cell[][] cells;
+	
+
     // TODO implements me #PROJECT #TUTO
 
     /**
@@ -17,6 +24,12 @@ public abstract class AreaBehavior
      * @param fileName (String): name of the file containing the behavior image, not null
      */
     public AreaBehavior(Window window, String fileName){
+    	
+    	//...
+    	/**
+    	* Each game will have its own Cell extension. */
+    	public abstract class Cell { //...
+    	}
     	
     }
 
